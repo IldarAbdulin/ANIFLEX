70e3f51be6fb3f4ab347e27f52809ebd474eaff2
IldarAbdulin <abdulinildar1@gmail.com>
2023-08-04 21:33:11 +0600

    created two modal windows for registration and login

--- src/components/home/home-main/HomeMain.tsx
+++ src/components/home/home-main/HomeMain.tsx
@@ -3,48 +3,78 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Logo from '../../../assets/home/logo.png';
 import { Button } from '../../../ui/home/Button';
+import { HomeLogin } from '../home-login/HomeLogin';
+import { HomeSignUp } from '../home-sign-up/HomeSignUp';
 
 AOS.init();
 
 const HomeMain: React.FC = () => {
+  const [isLoginActive, setIsLoginActive] = React.useState<boolean>(false);
+  const [isSignUpActive, setIsSignUpActive] = React.useState<boolean>(false);
   return (
-    <div className="home-main">
-      <div className="home-main__zerno">
-        <div className="home-main__shadow">
-          <div
-            data-aos="fade-down"
-            data-aos-duration="1000"
-            className="home-main__content"
-          >
-            <div className="home-main__content-header">
-              <div className="home-main__content-header_logo">
-                <img src={Logo} alt="logo" />
+    <>
+      <div className="home-main">
+        <div className="home-main__zerno">
+          <div className="home-main__shadow">
+            <div className="home-main__content">
+              <div
+                data-aos="fade-down"
+                data-aos-duration="500"
+                className="home-main__content-header"
+              >
+                <div className="home-main__content-header_logo">
+                  <img src={Logo} alt="logo" />
+                </div>
+                <div className="home-main__content-header_btns">
+                  <button onClick={() => setIsLoginActive(true)}>Вход</button>
+                  <button onClick={() => setIsSignUpActive(true)}>
+                    Регисрация
+                  </button>
+                </div>
               </div>
-              <div className="home-main__content-header_btns">
-                <button>Вход</button>
-                <button>Регисрация</button>
+              <div
+                data-aos="fade-up"
+                data-aos-duration="1000"
+                className="home-main__content-text_content"
+              >
+                <h1>
+                  Аниме, манга , манхва и многое другое совершенно{' '}
+                  <span>бесплатно</span>
+                </h1>
+                <p>
+                  Открой ворота в захватывающий мир аниме и соверши прыжок в
+                  неизведанные истории! Нажми на кнопку и отправляйся в
+                  атмосферу удивительных приключений!
+                </p>
+                <Button
+                  className="home-main__content-text_content-btn"
+                  title="Перейти на страницу с аниме"
+                  showArrow={true}
+                />
               </div>
-            </div>
-            <div className="home-main__content-text_content">
-              <h1>
-                Аниме, манга , манхва и многое другое совершенно{' '}
-                <span>бесплатно</span>
-              </h1>
-              <p>
-                Открой ворота в захватывающий мир аниме и соверши прыжок в
-                неизведанные истории! Нажми на кнопку и отправляйся в атмосферу
-                удивительных приключений!
-              </p>
-              <Button
-                className="home-main__content-text_content-btn"
-                title="Перейти на страницу с аниме"
-                showArrow={true}
-              />
+              {isLoginActive && (
+                <HomeLogin
+                  setActive={setIsLoginActive}
+                  onRegistrationFormActive={() => {
+                    setIsLoginActive(false);
+                    setIsSignUpActive(true);
+                  }}
+                />
+              )}
+              {isSignUpActive && (
+                <HomeSignUp
+                  setActive={setIsSignUpActive}
+                  onSignInFormActive={() => {
+                    setIsLoginActive(true);
+                    setIsSignUpActive(false);
+                  }}
+                />
+              )}
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
